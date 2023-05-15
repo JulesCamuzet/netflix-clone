c5beb3d23b3f8550912a93f528aa659d045c038f
juleau14 <jcamuzet@yahoo.com>
2023-05-15 20:31:54 +0200

    adding movieID as useEffect param

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -1,32 +1,8 @@
-import { useLocation, useParams } from "react-router-dom";
+import { useParams } from "react-router-dom";
 import "./MovieDetails.css";
 import getByID from "../../api/getByID";
 import { useEffect, useState } from "react";
 
-function getGenreNameFromId(id) {
-  const genres = {
-    28: "Action",
-    12: "Adventure",
-    16: "Animation",
-    35: "Comedy",
-    80: "Crime",
-    99: "Documentary",
-    18: "Drama",
-    10751: "Family",
-    14: "Fantasy",
-    36: "History",
-    27: "Horror",
-    10402: "Music",
-    9648: "Mystery",
-    10749: "Romance",
-    878: "Science Fiction",
-    10770: "TV Movie",
-    53: "Thriller",
-    10752: "War",
-    37: "Western",
-  };
-  return genres[id] || null;
-}
 
 const MovieDetails = () => {
   let { movieId } = useParams();
@@ -38,7 +14,7 @@ const MovieDetails = () => {
       let movieData = await getByID(movieId);
       setData(movieData);
     })();
-  }, []);
+  }, [movieId]);
 
   if (data === null) {
     return <div></div>;
